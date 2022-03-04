f5f04cd379c901a8f1b0eb82a53ffade468d3961
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-04 21:38:56 +0800

    убрана защита моделей

--- app/Models/ProjectTag.php
+++ app/Models/ProjectTag.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectTag extends Model
 {
     use HasFactory;
+
+    protected $guarded = false;
 }
--- app/Models/StateParticipation.php
+++ app/Models/StateParticipation.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class StateParticipation extends Model
 {
     use HasFactory;
+
+    protected $guarded = false;
 }
