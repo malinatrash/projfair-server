4c7a13758f1183efc93bdd4f0a78057a6dcc192b
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-04 21:14:07 +0800

    фронт

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <script type="module" crossorigin src="{{ asset('assets/index.14995303.js') }}"></script>
+    <link rel="modulepreload" href="{{ asset('assets/vendor.c7c73a5f.js') }}">
+    <link rel="stylesheet" href="{{ asset('assets/index.d849d6c5.css') }}">
+</head>
+
+<body class="antialiased">
+    <div id="app"></div>
+</body>
+
+</html>
