3b20d0ef4e8784ae702b162f5df5c1d3109666bc
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-04 22:48:14 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ApiAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Campus'], function () {
+    Route::get('/', 'LoginController');
     Route::get('/campus_auth', 'LoginController');
     Route::get('/campus_out', 'LogoutController')->middleware(ApiAuth::class);
 });
