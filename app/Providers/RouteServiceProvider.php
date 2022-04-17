09db97c78287d00a6956d0374186f9036326eb92
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-17 20:58:34 +0800

    get by id fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
-
+                ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
