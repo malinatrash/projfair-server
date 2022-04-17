6f9d09c8cba9e5b36f20603fae3325c608e17ce0
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-17 21:34:45 +0800

    check

--- app/Http/Controllers/Candidate/MeController.php
+++ app/Http/Controllers/Candidate/MeController.php
@@ -12,20 +12,20 @@ class MeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        dd($request);
-        $token = $request->get('api_token');
-        $id = Candidate::where('api_token', $token)->select('id')->get()[0]['id'];
-        $id_finalState = StateParticipation::where('state', 'Завершил')->select('id')->get()[0]['id'];
+        // dd($request);
+        // $token = $request->get('api_token');
+        // $id = Candidate::where('api_token', $token)->select('id')->get()[0]['id'];
+        // $id_finalState = StateParticipation::where('state', 'Завершил')->select('id')->get()[0]['id'];
 
-        $candidate = Candidate::where('api_token', $token)->get()[0];
+        // $candidate = Candidate::where('api_token', $token)->get()[0];
 
-        $participation = Participation::select('id_project')->where('id_candidate', $id)->where('id_state', $id_finalState)->pluck('id_project');
-        $candidate['experience'] = $participation;
+        // $participation = Participation::select('id_project')->where('id_candidate', $id)->where('id_state', $id_finalState)->pluck('id_project');
+        // $candidate['experience'] = $participation;
 
-        if ($candidate) {
-            return response()->json($candidate)->setStatusCode(200, 'Candidate');
-        } else {
-            return response()->json(['status' => false], 404);
-        }
+        // if ($candidate) {
+        //     return response()->json($candidate)->setStatusCode(200, 'Candidate');
+        // } else {
+        //     return response()->json(['status' => false], 404);
+        // }
     }
 }
