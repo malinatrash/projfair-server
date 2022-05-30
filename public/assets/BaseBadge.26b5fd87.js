f7a87c969958eb7286d5b2b7599bdf8c99f3c89b
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-05-30 14:03:20 +0800

    Новая версия фронта

--- public/assets/BaseBadge.26b5fd87.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index.8ed2e248.js";import{a as s,f as _,g as d}from"./vendor.bde10b0a.js";const r={},t={class:"badge"};function o(e,n){return s(),_("div",t,[d(e.$slots,"default",{},void 0,!0)])}var i=a(r,[["render",o],["__scopeId","data-v-2f36d946"]]);export{i as B};
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Ярмарка проектов ИРНИТУ</title>
 
-    <script type="module" crossorigin src="{{ asset('assets/index.8ed2e248.js') }}"></script>
-    <link rel="modulepreload" href="{{ asset('assets/vendor.bde10b0a.js') }}">
-    <link rel="stylesheet" href="{{ asset('assets/index.0e650290.css') }}">
+    <script type="module" crossorigin src="{{ asset('assets/index.d1195915.js') }}"></script>
+    <link rel="modulepreload" href="{{ asset('assets/vendor.38f2e4e5.js') }}">
+    <link rel="stylesheet" href="{{ asset('assets/index.3b736275.css') }}">
 
 </head>
 
