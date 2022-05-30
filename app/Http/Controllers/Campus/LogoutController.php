f9e42f5296b46a14f2f7706df38b724557f29cfe
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-05-30 17:08:09 +0800

    Удаление куки при выходе

--- app/Http/Controllers/Campus/LogoutController.php
+++ app/Http/Controllers/Campus/LogoutController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Candidate;
 use App\Models\Supervisor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class LogoutController extends Controller
 {
@@ -15,7 +16,8 @@ class LogoutController extends Controller
 
         Supervisor::where('api_token', $token)->update(['api_token' => null]);
         Candidate::where('api_token', $token)->update(['api_token' => null]);
-
+        $cookie = Cookie::queue(Cookie::forget('token'));
+        $cookie = Cookie::queue(Cookie::forget('is_student'));
         return redirect()->away('https://int.istu.edu/?logout=yes');
     }
 }
