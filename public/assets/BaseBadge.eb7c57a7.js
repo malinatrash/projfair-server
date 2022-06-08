1fbe833e5bf906f1526e1f08a2f752b65ac138bf
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-06-08 13:08:22 +0800

    fix front

---
+++ public/assets/BaseBadge.eb7c57a7.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index.241f548b.js";import{c as a,i as _,j as r}from"./vendor.66a6c8d1.js";const t={},d={class:"badge"};function o(e,c){return a(),_("div",d,[r(e.$slots,"default",{},void 0,!0)])}var f=s(t,[["render",o],["__scopeId","data-v-6645b90c"]]);export{f as B};
---
+++ public/assets/SidebarContainer.2b4b15fa.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index.241f548b.js";import{c as r,i as d,g as s,j as a}from"./vendor.66a6c8d1.js";const o={},t={class:"container"},i={class:"aside"};function _(e,c){return r(),d("div",t,[s("aside",i,[a(e.$slots,"sidebar",{},void 0,!0)]),s("div",null,[a(e.$slots,"main",{},void 0,!0)])])}var f=n(o,[["render",_],["__scopeId","data-v-a6b1bda2"]]);export{f as S};
