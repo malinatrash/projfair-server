9652c729cd00ef0514ef29b0dfb77350e2831904
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-10-02 21:08:58 +0800

    fix skill import

--- app/Http/Controllers/Admin/Skill/IndexController.php
+++ app/Http/Controllers/Admin/Skill/IndexController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers\Campus;
 
 use App\Http\Controllers\Controller;
-use App\Models\Candidate;
-use App\Models\Supervisor;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Str;
+use App\Models\Skill;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request)
     {
         $skills = Skill::all();
-        
+
         return [
             'skills' => $skills
-        ]
+        ];
     }
 }
