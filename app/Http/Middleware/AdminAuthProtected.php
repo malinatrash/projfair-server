6a7e1577338e7393ca455ba0953cc818ef726579
Александр Никульшеев <a.nikulsheev@gmail.com>
2023-02-02 16:24:27 +0800

    fix middleware admin name

--- app/Http/Middleware/AdminAuthProtected.php
+++ app/Http/Middleware/AdminAuthProtected.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class SupervisorAuthProtected
+class AdminAuthProtected
 {
     public function handle(Request $request, Closure $next)
     {
