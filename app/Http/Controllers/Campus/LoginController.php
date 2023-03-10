581b2a99edc379ab2e06154b1b2e72d69aa1e442
Александр Никульшеев <a.nikulsheev@gmail.com>
2023-03-10 20:28:42 +0800

    plsss

--- app/Http/Controllers/Campus/LoginController.php
+++ app/Http/Controllers/Campus/LoginController.php
@@ -35,15 +35,12 @@ class LoginController extends Controller
         if (env('APP_DEBUG')) {
             $role = $request->input('role');
             $token = $request->input('api_token');
-
+            $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
             $expiresTime = time() + (7 * 24 * 60 * 60);
-            $cookieOptions = ['httponly' => true, 'expires' => $expiresTime, 'domain' => false];
-            setcookie('is_student', $role == 'is_student', time() + 60 * 60 * 24 * 365, '/', false, false);
-            setcookie('is_teacher', $role == 'is_teacher', time() + 60 * 60 * 24 * 365, '/', false, false);
-            setcookie('token', $token, time() + 60 * 60 * 24 * 365, '/', false, false);
-            // setcookie('is_student', $role == 'is_student', $cookieOptions);
-            // setcookie('is_teacher', $role == 'is_teacher', $cookieOptions);
-            // setcookie('token', $token, $cookieOptions);
+            $cookieOptions = ['httponly' => true, 'expires' => $expiresTime, 'domain' => $domain, 'sameSite' => 'None'];
+            setcookie('is_student', $role == 'is_student', $cookieOptions);
+            setcookie('is_teacher', $role == 'is_teacher', $cookieOptions);
+            setcookie('token', $token, $cookieOptions);
             return json_encode(['url' => '']);
         }
 
