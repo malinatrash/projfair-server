51696c5407aa63ce85b0969162b180e97b610347
Александр Никульшеев <a.nikulsheev@gmail.com>
2023-03-26 13:25:09 +0800

    return

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,16 +51,6 @@
 
 <body class="antialiased">
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-
     <div id="app"></div>
 </body>
 
