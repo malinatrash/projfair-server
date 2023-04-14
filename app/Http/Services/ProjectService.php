0b9da83944e5c689970a99e6f02f606105048b94
Александр Никульшеев <a.nikulsheev@gmail.com>
2023-04-14 22:44:59 +0800

    use transactions project store

--- app/Http/Services/ProjectService.php
+++ app/Http/Services/ProjectService.php
@@ -7,136 +7,30 @@ use App\Models\ProjectSpeciality;
 use App\Models\ProjectSupervisor;
 use App\Models\Skill;
 use App\Models\Supervisor;
+use Illuminate\Support\Facades\DB;
 
 class ProjectService
 {
-    public function storeBySupervisor(Supervisor $supervisorCreator, $data): Project
+    public function storeBySupervisor(Supervisor $supervisorCreator, array $data): Project
     {
-        $supervisorCreatorRoleIds = [1];
-        $specialities = $data["specialities"];
-        $supervisors = $data["supervisors"];
-        unset($data['specialities']);
-        unset($data['supervisors']);
-
-        $skillIds = $data['skill_ids'];
-        $newSkillNames = $data['new_skills'];
-
-        unset($data['skill_ids']);
-        unset($data['new_skills']);
-        $project = Project::create($data);
-
-        foreach ($specialities as $speciality) {
-            ProjectSpeciality::create([
-                "project_id" => $project->id,
-                "speciality_id" => $speciality["specialitiy_id"],
-                "course" => $speciality["course"],
-                "priority" => $speciality["priority"]
-            ]);
-        }
-
-        foreach ($supervisors as $supervisor) {
-
-            if ($supervisor["supervisor_id"] == $supervisorCreator->id) {
-                $supervisorCreatorRoleIds = array_merge($supervisorCreatorRoleIds, $supervisor["role_ids"]);
-                continue;
-            }
-
-            $projectSupervisor = ProjectSupervisor::create([
-                "project_id" => $project->id,
-                "supervisor_id" => $supervisor["supervisor_id"]
-            ]);
-
-            $projectSupervisor->roles()->attach($supervisor["role_ids"]);
-        }
-
-        $projectSupervisorCreator = ProjectSupervisor::create([
-            "project_id" => $project->id,
-            "supervisor_id" => $supervisorCreator->id
-        ]);
-
-        $projectSupervisorCreator->roles()->attach($supervisorCreatorRoleIds);
-
-        foreach ($newSkillNames as $newSkillName) {
-            $skill = Skill::firstOrCreate([
-                "name" => $newSkillName,
-                "skillCategory_id" => 1,
-            ]);
-
-            array_push($skillIds, $skill->id);
-        }
-
-        $project->skills()->attach($skillIds);
-        return $project;
-    }
-    public function store()
-    {
+        $data['supervisors'] = $this->mergeSupervisorCreatorWithSupervisors($supervisorCreator, $data['supervisors']);
+        return $this->store($data);
     }
 
-    public function update()
+    public function store(array $data): Project
     {
-    }
-    public function updateBySupervisor(Supervisor $supervisorCreator, $data, Project $project): Project
-    {
-        $supervisorCreatorRoleIds = [1];
-
-        if (isset($data['skill_ids'])) {
-            $project->skills()->sync($data['skill_ids']);
-            unset($data['skill_ids']);
-        }
-
-        if (isset($data['new_skills'])) {
-            $newSkillIds = [];
-            $newSkillNames = $data['new_skills'];
-            unset($data['new_skills']);
-
-            foreach ($newSkillNames as $newSkillName) {
-                $skill = Skill::firstOrCreate([
-                    "name" => $newSkillName,
-                    "skillCategory_id" => 1,
-                ]);
-
-                array_push($newSkillIds, $skill->id);
-            }
-
-            $project->skills()->syncWithoutDetaching($newSkillIds);
-        }
-
-
-        if (isset($data['supervisors'])) {
-
-            ProjectSupervisor::where('project_id', $project->id)->delete();
-
-            $supervisors = $data['supervisors'];
+        return DB::transaction(function () use ($data) {
+            $specialities = $data["specialities"];
+            $supervisors = $data["supervisors"];
+            unset($data['specialities']);
             unset($data['supervisors']);
 
-            foreach ($supervisors as $supervisor) {
+            $skillIds = $data['skill_ids'];
+            $newSkillNames = $data['new_skills'];
 
-                if ($supervisor["supervisor_id"] == $supervisorCreator->id) {
-                    $supervisorCreatorRoleIds = array_merge($supervisorCreatorRoleIds, $supervisor["role_ids"]);
-                    continue;
-                }
-
-                $projectSupervisor = ProjectSupervisor::create([
-                    "project_id" => $project->id,
-                    "supervisor_id" => $supervisor["supervisor_id"]
-                ]);
-
-                $projectSupervisor->roles()->attach($supervisor["role_ids"]);
-            }
-            $projectSupervisorCreator = ProjectSupervisor::create([
-                "project_id" => $project->id,
-                "supervisor_id" => $supervisorCreator->id
-            ]);
-
-            $projectSupervisorCreator->roles()->attach($supervisorCreatorRoleIds);
-        }
-
-
-        if (isset($data['specialities'])) {
-            $specialities = $data['specialities'];
-            unset($data['specialities']);
-
-            ProjectSpeciality::where('project_id', $project->id)->delete();
+            unset($data['skill_ids']);
+            unset($data['new_skills']);
+            $project = Project::create($data);
 
             foreach ($specialities as $speciality) {
                 ProjectSpeciality::create([
@@ -146,10 +40,90 @@ class ProjectService
                     "priority" => $speciality["priority"]
                 ]);
             }
-        }
 
-        $project->update($data);
-        return $project;
+            foreach ($supervisors as $supervisor) {
+                $projectSupervisor = ProjectSupervisor::create([
+                    "project_id" => $project->id,
+                    "supervisor_id" => $supervisor["supervisor_id"]
+                ]);
+                $projectSupervisor->roles()->attach($supervisor["role_ids"]);
+            }
+
+            foreach ($newSkillNames as $newSkillName) {
+                $skill = Skill::firstOrCreate([
+                    "name" => $newSkillName,
+                    "skillCategory_id" => 1,
+                ]);
+                array_push($skillIds, $skill->id);
+            }
+
+            $project->skills()->attach($skillIds);
+            return $project;
+        });
+    }
+
+    public function updateBySupervisor(Supervisor $supervisorCreator, array $data, Project $project): Project
+    {
+        if (isset($data['supervisors'])) {
+            $data['supervisors'] = $this->mergeSupervisorCreatorWithSupervisors($supervisorCreator, $data['supervisors']);
+        }
+        return $this->update($data, $project);
+    }
+
+    public function update(array $data, Project $project): Project
+    {
+        return DB::transaction(function () use ($data, $project) {
+            if (isset($data['skill_ids'])) {
+                $project->skills()->sync($data['skill_ids']);
+                unset($data['skill_ids']);
+            }
+
+            if (isset($data['new_skills'])) {
+                $newSkillIds = [];
+                $newSkillNames = $data['new_skills'];
+                unset($data['new_skills']);
+                foreach ($newSkillNames as $newSkillName) {
+                    $skill = Skill::firstOrCreate([
+                        "name" => $newSkillName,
+                        "skillCategory_id" => 1,
+                    ]);
+                    array_push($newSkillIds, $skill->id);
+                }
+                $project->skills()->syncWithoutDetaching($newSkillIds);
+            }
+
+
+            if (isset($data['supervisors'])) {
+                ProjectSupervisor::where('project_id', $project->id)->delete();
+                $supervisors = $data['supervisors'];
+                unset($data['supervisors']);
+                foreach ($supervisors as $supervisor) {
+                    $projectSupervisor = ProjectSupervisor::create([
+                        "project_id" => $project->id,
+                        "supervisor_id" => $supervisor["supervisor_id"]
+                    ]);
+                    $projectSupervisor->roles()->attach($supervisor["role_ids"]);
+                }
+            }
+
+
+            if (isset($data['specialities'])) {
+                $specialities = $data['specialities'];
+                unset($data['specialities']);
+                ProjectSpeciality::where('project_id', $project->id)->delete();
+                foreach ($specialities as $speciality) {
+                    ProjectSpeciality::create([
+                        "project_id" => $project->id,
+                        "speciality_id" => $speciality["specialitiy_id"],
+                        "course" => $speciality["course"],
+                        "priority" => $speciality["priority"]
+                    ]);
+                }
+            }
+
+            $project->update($data);
+            return $project;
+        });
     }
 
     public function delete()
@@ -162,5 +136,27 @@ class ProjectService
 
     public function show()
     {
+    }
+
+    /** Вставить преподавателя-создателя проекта в массив преподавателей на проекте */
+    public function mergeSupervisorCreatorWithSupervisors(Supervisor $supervisorCreator, $supervisorsRoles): array
+    {
+        $hasSupervisorCreator = false;
+        $supervisorCreatorRoleId = 1;
+        foreach ($supervisorsRoles as $supervisor) {
+            if ($supervisor["supervisor_id"] == $supervisorCreator->id) {
+                $hasSupervisorCreator = true;
+                array_push($supervisor["role_ids"], $supervisorCreatorRoleId);
+                break;
+            }
+        }
+        if (!$hasSupervisorCreator) {
+            array_push($supervisorsRoles, [
+                "supervisor_id" => $supervisorCreator->id,
+                "role_ids" => [$supervisorCreatorRoleId]
+            ]);
+        }
+
+        return $supervisorsRoles;
     }
 }
--- tests/Feature/SupervisorTest.php
+++ tests/Feature/SupervisorTest.php
@@ -17,7 +17,53 @@ class SupervisorTest extends TestCase
     /** @test */
     public function create_project_with_harvesting()
     {
-        $this->assertTrue(true);
+        $createdProject = $this->post('api/supervisor/projects', [
+            "title" => "Пример названия",
+            "places" => 24,
+            "goal" => "string",
+            "description" => "string",
+            "difficulty" => 0,
+            "date_start" => "2022-11-27",
+            "date_end" => "2022-11-27",
+            "requirements" => "string",
+            "customer" => "string",
+            "product_result" => "string",
+            "study_result" => "string",
+            "additional_inf" => "string",
+            "type_id" => 1,
+            "department_id" => 10,
+
+            "prev_project_id" => 1,
+            "state_id" => 6,
+            "supervisors" => [
+                [
+                    "supervisor_id" => 2,
+                    "role_ids" => [2, 3]
+                ]
+            ],
+            "skill_ids" => [
+                1
+            ],
+            "new_skills" => [
+                "TETS1",
+                "TEST2"
+            ],
+            "specialities" => [
+                [
+                    "specialitiy_id" => 1,
+                    "course" => 2,
+                    "priority" => 1
+                ]
+            ]
+
+        ], [
+            "token" => 2
+        ]);
+
+
+        $this->assertEquals($createdProject['title'], 'Пример названия');
+        dd($createdProject['supervisors']);
+        $this->assertEquals($createdProject['supervisors'], 'Пример названия');
     }
 
     /** @test */
